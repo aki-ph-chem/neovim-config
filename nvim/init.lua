8fc089b05f9802a40a5a3175324d41e5d53ab15a
aki <aki@mymail.com>
2023-02-04 13:21:38 +0900

    現在のneovimの設定

--- nvim/init.lua
+++ nvim/init.lua
@@ -8,8 +8,24 @@ opt.number = true
 opt.clipboard = "unnamedplus" 
 opt.smartindent = true
 opt.shiftwidth = 4
+opt.expandtab = true
 
-vim.cmd[[colorscheme desert]]
+
+-- vim.cmd[[colorscheme desert]]
+-- vim.cmd[[colorscheme default]]
+ vim.cmd[[colorscheme codedark]]
+
+vim.cmd[[highlight Normal ctermbg=none]] 
+vim.cmd[[highlight NonText ctermbg=none]]   
+vim.cmd[[highlight LineNr ctermbg=none]]
+vim.cmd[[highlight Folded ctermbg=none]]
+vim.cmd[[highlight EndOfBuffer ctermbg=none]]
+
+
+-- latex syntax
+
+vim.cmd[[let g:tex_conceal = '']]
+
 --opt.syntax = true
 
 --- for key map
@@ -23,3 +39,146 @@ local function map(mode, lhs, rhs, opts)
 end
 
 map('i','jj','<Esc>')
+--map('v','vv','<C-v>')
+
+
+vim.cmd[[let $BASH_ENV = "~/.bash_aliases"]]
+
+
+-- cargo run
+vim.cmd[[command Car !cargo run]]
+
+
+-- mason: LSPマネージャー
+require("mason").setup()
+require("mason-lspconfig").setup()
+
+-- python
+require("lspconfig").pyright.setup {}
+-- cpp
+require("lspconfig").clangd.setup {}
+-- Rust
+require("lspconfig").rust_analyzer.setup {}
+-- Lua
+require("lspconfig").sumneko_lua.setup {}
+
+
+-- 補完:nvim-cmpのgithubのコピペ
+  local cmp = require'cmp'
+
+  cmp.setup({
+    snippet = {
+      -- REQUIRED - you must specify a snippet engine
+      expand = function(args)
+        vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+        -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+        -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+        -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+      end,
+    },
+    window = {
+      -- completion = cmp.config.window.bordered(),
+      -- documentation = cmp.config.window.bordered(),
+    },
+    mapping = cmp.mapping.preset.insert({
+      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      ['<C-Space>'] = cmp.mapping.complete(),
+      ['<C-e>'] = cmp.mapping.abort(),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    }),
+    sources = cmp.config.sources({
+      { name = 'nvim_lsp' },
+      { name = 'vsnip' }, -- For vsnip users.
+      -- { name = 'luasnip' }, -- For luasnip users.
+      -- { name = 'ultisnips' }, -- For ultisnips users.
+      -- { name = 'snippy' }, -- For snippy users.
+    }, {
+      { name = 'buffer' },
+    })
+  })
+
+  -- Set configuration for specific filetype.
+  cmp.setup.filetype('gitcommit', {
+    sources = cmp.config.sources({
+      { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
+    }, {
+      { name = 'buffer' },
+    })
+  })
+
+  -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+  cmp.setup.cmdline({ '/', '?' }, {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+      { name = 'buffer' }
+    }
+  })
+
+  -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+  cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+      { name = 'path' }
+    }, {
+      { name = 'cmdline' }
+    })
+  })
+  cmp.setup({
+    snippet = {
+      -- REQUIRED - you must specify a snippet engine
+      expand = function(args)
+        vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+        -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+        -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+        -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+      end,
+    },
+    window = {
+      -- completion = cmp.config.window.bordered(),
+      -- documentation = cmp.config.window.bordered(),
+    },
+    mapping = cmp.mapping.preset.insert({
+      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      ['<C-Space>'] = cmp.mapping.complete(),
+      ['<C-e>'] = cmp.mapping.abort(),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    }),
+    sources = cmp.config.sources({
+      { name = 'nvim_lsp' },
+      { name = 'vsnip' }, -- For vsnip users.
+      -- { name = 'luasnip' }, -- For luasnip users.
+      -- { name = 'ultisnips' }, -- For ultisnips users.
+      -- { name = 'snippy' }, -- For snippy users.
+    }, {
+      { name = 'buffer' },
+    })
+  })
+
+  -- Set configuration for specific filetype.
+  cmp.setup.filetype('gitcommit', {
+    sources = cmp.config.sources({
+      { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
+    }, {
+      { name = 'buffer' },
+    })
+  })
+
+  -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+  cmp.setup.cmdline({ '/', '?' }, {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+      { name = 'buffer' }
+    }
+  })
+
+  -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+  cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+      { name = 'path' }
+    }, {
+      { name = 'cmdline' }
+    })
+  })
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,5 +1,30 @@
+
+-- require().startup()の構文はLuaのものではなくてneovimの拡張??
 require("packer").startup(function(use)
 
     use 'cohama/lexima.vim'
-    use 'Yggdroot/indentLine'
+    -- indentLineがインストールされているとconcealの挙動がおかしくなるから一時的に消した
+    -- use 'Yggdroot/indentLine'
+    use 'lervag/vimtex'
+
+    --- for GNU GLOBAL
+    use 'vim-scripts/gtags.vim'
+
+    -- vs-code風シンタックス
+    use 'tomasiser/vim-code-dark'
+
+    -- mason.nvim: LSP関係
+    use {
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        "neovim/nvim-lspconfig",
+    }
+
+    -- コード補完
+     use {
+        "hrsh7th/nvim-cmp",
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/vim-vsnip",
+     } 
+
 end)
