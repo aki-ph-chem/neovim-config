cbe8108a6b3d86bd84ceea4ca60555ddb63fee53
aki <aki@mymail.com>
2023-12-20 22:03:55 +0900

    add config for rust

--- nvim/init.lua
+++ nvim/init.lua
@@ -182,8 +182,30 @@ require("lspconfig").clangd.setup {
 
 -- CMake
 require("lspconfig").cmake.setup {}
+
 -- Rust
-require("lspconfig").rust_analyzer.setup {}
+require("lspconfig").rust_analyzer.setup({
+    on_attach=on_attach,
+    settings = {
+        ["rust-analyzer"] = {
+            imports = {
+                granularity = {
+                    group = "module",
+                },
+                prefix = "self",
+            },
+            cargo = {
+                buildScripts = {
+                    enable = true,
+                },
+            },
+            procMacro = {
+                enable = true
+            },
+        }
+    }
+})
+
 -- go lang
 require("lspconfig").gopls.setup {}
 -- Lua
