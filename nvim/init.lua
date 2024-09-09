16d5642ff44708d59132fecf7d6abaccd4bb9c35
aki <aki@mymail.com>
2024-09-10 00:58:26 +0900

    fix init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -115,9 +115,10 @@ vim.api.nvim_create_user_command("Dfc", function()
 end, { nargs = 0 })
 
 -- config eskk
-vim.cmd([[let g:eskk#directory = "/usr/share/skk"]])
+vim.cmd([[let g:eskk#directory = "~/.eskk"]])
 vim.cmd([[let g:eskk#dictionary = { 'path': "~/.config/eskk/my_jisyo", 'sorted': 1, 'encoding': 'utf-8',}]])
 vim.cmd([[let g:eskk#large_dictionary = {'path': "/usr/share/skk/SKK-JISYO.L", 'sorted': 1, 'encoding': 'euc-jp',} ]])
+vim.cmd([[let g:eskk#enable_completion = 1]])
 
 -- LSP config
 -- mason: LSP manager
