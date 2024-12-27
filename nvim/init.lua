52f6b7bce7d217ce5771d7f4d13d26d146c2a130
aki <aki@mymail.com>
2024-12-27 15:40:08 +0900

    fix

--- nvim/init.lua
+++ nvim/init.lua
@@ -193,8 +193,8 @@ local use_lsp_log_debug = false
 if use_lsp_log_debug then
 	vim.lsp.set_log_level("debug")
 end
---- jump to definition by 'jd'
-vim.keymap.set("n", "jd", "<cmd>:lua vim.lsp.buf.definition()<CR>")
+--- jump to definition by 'gd'
+vim.keymap.set("n", "gd", "<cmd>:lua vim.lsp.buf.definition()<CR>")
 
 -- LSP config
 -- mason: LSP manager
