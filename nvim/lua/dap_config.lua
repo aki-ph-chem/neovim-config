6858285c223e6455b8490e0f6d380f64adebe7ad
aki <aki@mymail.com>
2025-01-28 02:23:34 +0900

    Pythonをdebugpyでdebugできるようにした

--- nvim/lua/dap_config.lua
+++ nvim/lua/dap_config.lua
@@ -7,6 +7,14 @@ dap.adapters = {
     type = 'executable',
     command = 'codelldb',
   },
+  debugpy = {
+    type = 'executable',
+    command = vim.fn.getcwd() .. '/.venv/bin/python',
+    args = { '-m', 'debugpy.adapter' },
+    options = {
+      source_filetype = 'python',
+    },
+  },
 }
 
 dap.configurations = {
@@ -33,6 +41,28 @@ dap.configurations = {
       end,
       cwd = '${workspaceFolder}',
       stopOnEntry = false,
+    },
+  },
+
+  python = {
+    {
+      name = 'Launch file',
+      type = 'debugpy',
+      request = 'launch',
+      program = '${file}',
+      pythonPath = function()
+        -- debugpy supports launching an application with a different interpreter then the one used to launch debugpy itself.
+        -- The code below looks for a `venv` or `.venv` folder in the current directly and uses the python within.
+        -- You could adapt this - to for example use the `VIRTUAL_ENV` environment variable.
+        local cwd = vim.fn.getcwd()
+        if vim.fn.executable(cwd .. '/venv/bin/python') == 1 then
+          return cwd .. '/venv/bin/python'
+        elseif vim.fn.executable(cwd .. '/.venv/bin/python') == 1 then
+          return cwd .. '/.venv/bin/python'
+        else
+          return '/usr/bin/python'
+        end
+      end,
     },
   },
 }
