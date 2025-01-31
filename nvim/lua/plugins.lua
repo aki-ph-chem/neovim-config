238191c78f254e387b0f3d7008f37ab98060635c
aki <aki@mymail.com>
2025-02-01 02:27:22 +0900

    lazygitをNeovimから使えるようにした

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -21,6 +21,26 @@ local fmt_plugins = {
 -- for git
 local git_plugins = {
   'tpope/vim-fugitive',
+  {
+    'kdheepak/lazygit.nvim',
+    lazy = true,
+    cmd = {
+      'LazyGit',
+      'LazyGitConfig',
+      'LazyGitCurrentFile',
+      'LazyGitFilter',
+      'LazyGitFilterCurrentFile',
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+      { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
+    },
+  },
 }
 
 -- barbar.nvim
