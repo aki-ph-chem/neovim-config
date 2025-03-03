3832723b9b5cc872c32c3c97fb00c72aaf04d4da
aki <aki@mymail.com>
2025-03-04 00:48:04 +0900

    markdown-oxideを導入した

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -190,6 +190,23 @@ require('vimscript_ls')
 -- julia
 require('lspconfig').julials.setup({})
 
+-- for markdown-oxide
+-- An example nvim-lspconfig capabilities setting
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
+
+require('lspconfig').markdown_oxide.setup({
+  -- Ensure that dynamicRegistration is enabled! This allows the LS to take into account actions like the
+  -- Create Unresolved File code action, resolving completions for unindexed code blocks, ...
+  capabilities = vim.tbl_deep_extend('force', capabilities, {
+    workspace = {
+      didChangeWatchedFiles = {
+        dynamicRegistration = true,
+      },
+    },
+  }),
+  --on_attach = on_attach, -- configure your on attach config
+})
+
 -- cmp config
 local cmp = require('cmp')
 cmp.setup({
