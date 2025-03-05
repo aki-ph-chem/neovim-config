27c4f685f5999fedc41c5e87ebfd3638cabd487f
aki <aki@mymail.com>
2025-03-05 15:36:41 +0900

    neovideの設定を追加

--- nvim/lua/neovide_config.lua
+++ nvim/lua/neovide_config.lua
@@ -1,4 +1,15 @@
+-- Ref: https://neovide.dev/configuration.html
 if vim.g.neovide then
+  -- refresh rate
+  vim.g.neovide_refresh_rate = 60
+
   -- background transparent
   vim.g.neovide_transparency = 0.85
+  vim.g.neovide_normal_opacity = 0.85
+
+  -- animation time for cursor position
+  vim.g.neovide_position_animation_length = 0.3
+
+  -- animation length
+  vim.g.neovide_cursor_animation_length = 0.02
 end
