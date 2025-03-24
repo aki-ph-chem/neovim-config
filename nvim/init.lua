acbaf070eed58a5b7bb2ce91cb5a9e85ac0dc568
aki <aki@mymail.com>
2025-03-24 18:36:43 +0900

    <leader>xでバッファの一括削除

--- nvim/init.lua
+++ nvim/init.lua
@@ -35,12 +35,22 @@ end
 map('i', 'jj', '<Esc>')
 --map('v','vv','<C-v>')
 
--- config for barbar
+-- config for barbar(buffer)
 -- Move to previous/next
 map('n', '<C-p>', '<Cmd>BufferPrevious<CR>', opts)
 map('n', '<C-n>', '<Cmd>BufferNext<CR>', opts)
 -- Close buffer
 map('n', '<leader>e', '<Cmd>BufferClose<CR>', opts)
+
+-- delete all buffer
+vim.keymap.set('n', '<leader>x', function()
+  for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
+    -- Delete only if buffer is loaded and unchanged
+    if vim.api.nvim_buf_is_loaded(bufnr) and not vim.api.nvim_buf_get_option(bufnr, 'modified') then
+      vim.api.nvim_buf_delete(bufnr, { force = true })
+    end
+  end
+end)
 
 vim.cmd([[let $BASH_ENV = "~/.bash_aliases"]])
 
