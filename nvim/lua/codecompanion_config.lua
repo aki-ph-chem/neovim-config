c6ba9294591800622c448b6e8d24b392097e0818
aki <aki@mymail.com>
2025-06-19 16:25:57 +0900

    codecompanion.nvim: For now, I've configured it to communicate in Japanese using Gemini.

--- nvim/lua/codecompanion_config.lua
+++ nvim/lua/codecompanion_config.lua
@@ -1,4 +1,28 @@
 require('codecompanion').setup({
-  adapter = {},
-  strategies = {},
+  adapters = {
+    gemini = function()
+      return require('codecompanion.adapters').extend('gemini', {
+        env = { api_key = vim.env.GEMINI_API_KEY },
+      })
+    end,
+  },
+  opts = {
+    language = 'Japanese',
+  },
+  strategies = {
+    chat = {
+      adapter = 'gemini',
+    },
+    inline = {
+      adapter = 'gemini',
+    },
+    agent = {
+      adapter = 'gemini',
+    },
+  },
 })
+
+-- Toggle chat
+vim.api.nvim_create_user_command('Cct', function()
+  vim.cmd(':CodeCompanionChat Toggle')
+end, { nargs = 0 })
