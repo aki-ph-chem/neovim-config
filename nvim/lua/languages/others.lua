7f3e6f339516ca3fd82ec0434bc99c041f2b8cc6
aki <aki@mymail.com>
2025-10-05 12:47:11 +0900

    separation of config for JS/TS to languages/ts.lua

--- nvim/lua/languages/others.lua
+++ nvim/lua/languages/others.lua
@@ -14,10 +14,6 @@ vim.lsp.enable({ 'nil_ls' })
 vim.lsp.config.teal_ls = {}
 vim.lsp.enable({ 'teal_ls' })
 
--- JavaScript
-vim.lsp.config.ts_ls = {}
-vim.lsp.enable({ 'ts_ls' })
-
 --  HTML
 vim.lsp.config.html = {}
 vim.lsp.enable({ 'html' })
---
+++ nvim/lua/languages/ts.lua
@@ -0,0 +1,22 @@
+local utl = require('languages.utl')
+
+-- languages server
+vim.lsp.config.ts_ls = {
+  settings = {},
+  on_attach = function(client, bufnr)
+    local current_dir = vim.fn.getcwd()
+    vim.env.PATH = current_dir .. '/node_modules/.bin:' .. vim.env.PATH
+    utl.navic.attach(client, bufnr)
+  end,
+}
+vim.lsp.enable({ 'ts_ls' })
+
+--require('formatter.filetypes').javascript = { require('formatter.filetypes.javascript').biome }
+require('formatter').setup({
+  filetype = {
+    javascript = { require('formatter.filetypes.javascript').biome },
+    javascriptreact = { require('formatter.filetypes.javascriptreact').biome },
+    typescript = { require('formatter.filetypes.typescript').biome },
+    typescriptreact = { require('formatter.filetypes.typescriptreact').biome },
+  },
+})
