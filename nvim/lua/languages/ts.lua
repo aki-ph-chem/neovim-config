75475436d62319353daf28cb5277710d96ee0eb7
aki <aki@mymail.com>
2025-10-14 02:42:03 +0900

    I set up the emf configuration for JS for now: But it doesn't work. #12

--- nvim/lua/languages/ts.lua
+++ nvim/lua/languages/ts.lua
@@ -9,11 +9,57 @@ vim.lsp.config.ts_ls = {
 }
 vim.lsp.enable({ 'ts_ls' })
 
-require('formatter').setup({
-  filetype = {
-    javascript = { require('formatter.filetypes.javascript').biome },
-    javascriptreact = { require('formatter.filetypes.javascriptreact').biome },
-    typescript = { require('formatter.filetypes.typescript').biome },
-    typescriptreact = { require('formatter.filetypes.typescriptreact').biome },
-  },
-})
+local use_efm = true
+
+if not use_efm then
+  require('formatter').setup({
+    filetype = {
+      javascript = { require('formatter.filetypes.javascript').biome },
+      javascriptreact = { require('formatter.filetypes.javascriptreact').biome },
+      typescript = { require('formatter.filetypes.typescript').biome },
+      typescriptreact = { require('formatter.filetypes.typescriptreact').biome },
+    },
+  })
+end
+
+if use_efm then
+  -- config for Biome formatter
+  local biome_formatter = {
+    formatCommand = 'biome format --stdin-file-path ${INPUT} --skip-project',
+    formatStdin = true,
+  }
+
+  -- config for efm-langserver by vim.lsp.config
+  vim.lsp.config.efm = {
+    cmd = { 'efm-langserver' },
+
+    settings = {
+      languages = {
+        javascript = { biome_formatter },
+        javascriptreact = { biome_formatter },
+        typescript = { biome_formatter },
+        typescriptreact = { biome_formatter },
+      },
+      rootMarkers = {
+        '.git/',
+        'package.json',
+        'biome.json',
+      },
+    },
+
+    init_options = {
+      documentFormatting = true,
+      documentRangeFormatting = true,
+    },
+
+    filetypes = {
+      'javascript',
+      'javascriptreact',
+      'typescript',
+      'typescriptreact',
+    },
+  }
+
+  -- activate efm
+  vim.lsp.enable({ 'efm' })
+end
