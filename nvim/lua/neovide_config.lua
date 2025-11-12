3a8cda15fe45901b4cd5bbcaf18ede43288803eb
aki <aki@mymail.com>
2025-11-13 01:21:11 +0900

    fix: Settings should now take effect even when attached as a UI to nvim.

--- nvim/lua/neovide_config.lua
+++ nvim/lua/neovide_config.lua
@@ -1,15 +1,17 @@
 -- Ref: https://neovide.dev/configuration.html
-if vim.g.neovide then
-  -- refresh rate
-  vim.g.neovide_refresh_rate = 60
+vim.api.nvim_create_autocmd('UIEnter', {
+  callback = function()
+    -- refresh rate
+    vim.g.neovide_refresh_rate = 60
 
-  -- background transparent
-  vim.g.neovide_opacity = 0.85
-  vim.g.neovide_normal_opacity = 0.85
+    -- background transparent
+    vim.g.neovide_opacity = 0.85
+    vim.g.neovide_normal_opacity = 0.85
 
-  -- animation time for cursor position
-  vim.g.neovide_position_animation_length = 0.3
+    -- animation time for cursor position
+    vim.g.neovide_position_animation_length = 0.3
 
-  -- animation length
-  vim.g.neovide_cursor_animation_length = 0.02
-end
+    -- animation length
+    vim.g.neovide_cursor_animation_length = 0.02
+  end,
+})
