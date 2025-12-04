c76dd236882851eb96b98e4d86c89b6480b93721
aki <lmxox@proton.me>
2025-12-04 12:03:14 +0900

    fix: set neovide back ground color

--- nvim/lua/neovide_config.lua
+++ nvim/lua/neovide_config.lua
@@ -11,6 +11,16 @@ vim.api.nvim_create_autocmd('UIEnter', {
     -- animation time for cursor position
     vim.g.neovide_position_animation_length = 0.3
 
+    -- RGB(50%,50%,50%)
+    local rgba = {
+      r = math.floor(255 * 0.5),
+      g = math.floor(255 * 0.5),
+      b = math.floor(255 * 0.5),
+    }
+    local rgba_string =
+      string.format('#%x%x%x%x', rgba.r, rgba.g, rgba.b, math.floor(255 * (vim.g.transparency or 0.5)))
+    vim.g.neovide_background_color = rgba_string
+
     -- animation length
     vim.g.neovide_cursor_animation_length = 0.02
   end,
