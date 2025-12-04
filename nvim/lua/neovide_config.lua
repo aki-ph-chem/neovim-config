f6fb1b4ae2c22d6c4335be36fe9a7c41f30cf1e0
aki <lmxox@proton.me>
2025-12-04 13:54:07 +0900

    fix: refactor neovide config

--- nvim/lua/neovide_config.lua
+++ nvim/lua/neovide_config.lua
@@ -1,27 +1,32 @@
 -- Ref: https://neovide.dev/configuration.html
+local config_neovide = function()
+  -- refresh rate
+  vim.g.neovide_refresh_rate = 60
+
+  -- background transparent
+  vim.g.neovide_opacity = 0.85
+  vim.g.neovide_normal_opacity = 0.85
+
+  -- animation time for cursor position
+  vim.g.neovide_position_animation_length = 0.3
+
+  -- RGB(50%,50%,50%)
+  local rgba = {
+    r = math.floor(255 * 0.5),
+    g = math.floor(255 * 0.5),
+    b = math.floor(255 * 0.5),
+  }
+  local rgba_string = string.format('#%x%x%x%x', rgba.r, rgba.g, rgba.b, math.floor(255 * (vim.g.transparency or 0.5)))
+  vim.g.neovide_background_color = rgba_string
+
+  -- animation length
+  vim.g.neovide_cursor_animation_length = 0.02
+end
+
 vim.api.nvim_create_autocmd('UIEnter', {
   callback = function()
-    -- refresh rate
-    vim.g.neovide_refresh_rate = 60
-
-    -- background transparent
-    vim.g.neovide_opacity = 0.85
-    vim.g.neovide_normal_opacity = 0.85
-
-    -- animation time for cursor position
-    vim.g.neovide_position_animation_length = 0.3
-
-    -- RGB(50%,50%,50%)
-    local rgba = {
-      r = math.floor(255 * 0.5),
-      g = math.floor(255 * 0.5),
-      b = math.floor(255 * 0.5),
-    }
-    local rgba_string =
-      string.format('#%x%x%x%x', rgba.r, rgba.g, rgba.b, math.floor(255 * (vim.g.transparency or 0.5)))
-    vim.g.neovide_background_color = rgba_string
-
-    -- animation length
-    vim.g.neovide_cursor_animation_length = 0.02
+    if vim.g.neovide then
+      config_neovide()
+    end
   end,
 })
