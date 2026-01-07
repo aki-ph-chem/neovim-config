7e74c8a917e1fb237c5fcd493ba463d4bbb1e0a4
aki <lmxox@proton.me>
2026-01-07 12:50:51 +0900

    feat/resession-extensions: OK in this time

--- nvim/lua/resession/extensions/myplugin.lua
+++ nvim/lua/resession/extensions/myplugin.lua
@@ -7,15 +7,7 @@ end
 
 M.on_post_load = function(data)
   print('data.project_dir = ' .. data.project_dir)
-  --vim.api.nvim_set_current_dir(data.project_dir)
-  --vim.system({ 'direnv', 'export', 'vim' }, { text = true, cwd = data.porject_dir }, on_exit)
-
-  --[[
-  vim.b.direnv_autoload_triggered = true
-  vim.defer_fn(function()
-    vim.b.direnv_autoload_triggered = false
-  end, 1000)
-  --]]
+  require('direnv-nvim').hook()
 end
 
 return M
