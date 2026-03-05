462ccfad779e322470a50ea7cbfa3c9388cf9a46
aki <lmxox@proton.me>
2026-03-06 03:41:27 +0900

    feat/gitsign-config: set Next Hunk, Prev Hunk, by ']c' and '[c'

--- nvim/lua/git_config.lua
+++ nvim/lua/git_config.lua
@@ -49,6 +49,38 @@ require('gitsigns').setup({
     row = 0,
     col = 1,
   },
+
+  on_attach = function(bufnr)
+    local gs = package.loaded.gitsigns
+
+    local function map(mode, l, r, opts)
+      opts = opts or {}
+      opts.buffer = bufnr
+      vim.keymap.set(mode, l, r, opts)
+    end
+
+    -- jump to next Hunk
+    map('n', ']c', function()
+      if vim.wo.diff then
+        return ']c'
+      end
+      vim.schedule(function()
+        gs.next_hunk()
+      end)
+      return '<Ignore>'
+    end, { expr = true, desc = 'Next Hunk' })
+
+    -- jump to previous Hunk
+    map('n', '[c', function()
+      if vim.wo.diff then
+        return '[c'
+      end
+      vim.schedule(function()
+        gs.prev_hunk()
+      end)
+      return '<Ignore>'
+    end, { expr = true, desc = 'Prev Hunk' })
+  end,
 })
 
 -- refresh Gitsings
