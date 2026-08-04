0163eebfd576d692348312028df295c7e952bf2f
aki <lmxox@proton.me>
2026-08-04 23:43:11 +0900

    feat: format terraform src by save

--- nvim/lua/languages/others.lua
+++ nvim/lua/languages/others.lua
@@ -57,3 +57,20 @@ vim.lsp.config.tofu_ls = {
   root_markers = { '.terraform', '.git' },
 }
 vim.lsp.enable('tofu_ls')
+
+-- format via LSP
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(args)
+    local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+    -- Auto-format on save
+    if client:supports_method('textDocument/formatting') then
+      vim.api.nvim_create_autocmd('BufWritePre', {
+        group = vim.api.nvim_create_augroup('tofu-ls', { clear = false }),
+        buffer = args.buf,
+        callback = function()
+          vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
+        end,
+      })
+    end
+  end,
+})
